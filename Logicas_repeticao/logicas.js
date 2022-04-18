01fde55988ea5d80053a4e757e5da13898d7e462
Leandro Lemos <leandrolemosj25@gmail.com>
2022-04-18 20:28:47 -0300

    ajustes cálculos e formato de números

--- Logicas_repeticao/logicas.js
+++ Logicas_repeticao/logicas.js
@@ -1,10 +1,16 @@
 function Converter() {
     var valorMetros = parseFloat(document.getElementById("valor").value);
-    var valorAlqueires = valorMetros / 24200;
-    var valorLitros = valorMetros / 605;
-    var resultadoAlqueire = document.getElementById("valorConvertidoalqueire");
-    var resultadoConversão = " O resultado de " + "<br>"+ valorMetros + " m² em alqueire é " + valorAlqueires.toFixed(3);
-    resultadoAlqueire.innerHTML = resultadoConversão
+    var valorAlqueires = Math.floor(valorMetros / 24200);
+    var valorLitros = (valorMetros % 24200)/605;
+    
+    var resultadoAlqueire = document.getElementById("valorConvertidoAlqueire");
+    var resultadoLitros = document.getElementById("valorConvertidoLitros");
+    var resultadoConversão = " O resultado de " + "<br>"+ valorMetros + " m² é " + valorAlqueires + " alqueire e " + valorLitros.toFixed(1) + " litros";
+    if (valorAlqueires >= 1) {
+    resultadoAlqueire.innerHTML = resultadoConversão}
+    else {
+        resultadoAlqueire.innerHTML= "O resultado de " + valorMetros + " m² é " + valorLitros.toFixed(2) + " litros"
+    }
 
-
+ 
 }
