714918abc10e7a0c98718edaf730519b86774789
Leandro Lemos <leandrolemosj25@gmail.com>
2022-08-15 20:19:36 -0300

    refatoração e ajuste para salvar itens no localstorage

--- APIcep/src/js/main.js
+++ APIcep/src/js/main.js
@@ -5,7 +5,7 @@ function copiarTexto() {
     textoCopiado.select();
     textoCopiado.setSelectionRange(0, 99999)
     document.execCommand("copy");
-    
+
 }
 
 
@@ -16,11 +16,11 @@ const cep = document.querySelector("#cep")
 
 const showData = (result) => {
     // para cada elemento armazena em variável campo
-    for (const campo in result){
+    for (const campo in result) {
         // se existe concatena com campo, se não ignora
-        if (document.querySelector("#"+campo)){
+        if (document.querySelector("#" + campo)) {
             //preencher o campo do form
-            document.querySelector("#"+campo).value = result[campo]
+            document.querySelector("#" + campo).value = result[campo]
         }
     }
 }
@@ -28,7 +28,7 @@ const showData = (result) => {
 
 
 cep.addEventListener("blur", (e) => {
-    let search = cep.value.replace("-","")
+    let search = cep.value.replace("-", "")
     //constantes de acesso a url
     const options = {
         method: "GET",
@@ -36,75 +36,83 @@ cep.addEventListener("blur", (e) => {
         cache: "default"
     }
     fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    //isso é uma promisse assincrona, se der certo .then 
-        .then((response)=> {response.json()
+        //isso é uma promisse assincrona, se der certo .then 
+        .then((response) => {
+            response.json()
             .then(data => showData(data))
-    
-    })
 
-    //se der erro .catch
-    .catch(e => console.log("Deu erro" + e, message))
+        })
 
-    
-} )
+        //se der erro .catch
+        .catch(e => console.log("Deu erro" + e, message))
+
+
+})
 
 // funções salvar item de forma dinamica
 
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const arrayItens = [] // para agregar os itens de localstorage
+// para agregar os itens de localstorage. Consulta, converte para objeto com o parser ou retorna um array vazio se ainda não houver nada. Item chave dentro do localstorage
+const arrayItens = JSON.parse(localStorage.getItem("itens")) || []
+
+//percorrendo o array retornado como object (parse) do localstorage >> chama o cria elemento. Recebe apenas mesmo o objeto elemento
+arrayItens.forEach((elemento) => {
+    criaEndereco(elemento)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-   // console.log(evento)
+    // console.log(evento)
     // variáveis que capturam dados do target
     const cep = evento.target.elements['cep']
     const logradouro = evento.target.elements['logradouro']
     const bairro = evento.target.elements['bairro']
     const localidade = evento.target.elements['localidade']
     const uf = evento.target.elements['uf']
-    //capturando dados de target> elements do console (envio form) e inserindo como param. da função
-    criaEndereco(cep.value, logradouro.value, bairro.value, localidade.value, uf.value)
+     // criando o objeto
+    //na refatoração começou a capturar o .value
+    const itemAtual = {
+        "cep": cep.value,
+        "logradouro": logradouro.value,
+        "bairro": bairro.value,
+        "localidade": localidade.value,
+        "uf": uf.value
+    }
+    
+    //na refatoração recebe o objeto como param
+    criaEndereco(itemAtual)
+    // operações localStorage
+   
+    
+    arrayItens.push(itemAtual) //inserindo dentro do array
+    //JSON.stringfy transforma o objeto em texto
+    localStorage.setItem("itens", JSON.stringify(arrayItens))
+
     this.limparCamposForm()
 })
 
-function criaEndereco (cep,logradouro,bairro,localidade,uf) {
-// cria o elemento li do html
-const novoItem = document.createElement('li')
-novoItem.classList.add('item', 'itemLista', 'itemLi')
-//insere um elemento dentro do elemento object para manipular
-const numCep = document.createElement('strong')
-numCep.innerHTML = cep
+function criaEndereco(item) {
+    // cria o elemento li do html
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item', 'itemLista', 'itemLi')
+    //insere um elemento dentro do elemento object para manipular
+    const numCep = document.createElement('strong')
+    numCep.innerHTML = item.cep // elemento item na posição cep (chave)
 
-novoItem.appendChild(numCep)
-novoItem.innerHTML += "(CEP), " + logradouro + ", " + bairro + ", " + localidade + ", " + uf + "."
+    novoItem.appendChild(numCep)
+    novoItem.innerHTML += "(CEP), " + item.logradouro + ", " + item.bairro + ", " + item.localidade + ", " + item.uf + "." // elemento captura da chave "bairro" do objeto
 
-lista.appendChild(novoItem)
-
-// operações localStorage
-// criando o objeto
-const itemAtual = {
-    "cep":cep,
-    "logradouro": logradouro,
-    "bairro": bairro,
-    "localidade": localidade,
-    "uf": uf
-}
-arrayItens.push(itemAtual) //inserindo dentro do array
-//JSON.stringfy transforma o objeto em texto
-localStorage.setItem("item", JSON.stringify(arrayItens))
-
-
-
+    lista.appendChild(novoItem)
 }
 
 function limparCamposForm() {
 
     document.getElementById('cep').value = '';
-    document.getElementById('logradouro').value = ''; 
-    document.getElementById('bairro').value = ''; 
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
     document.getElementById('localidade').value = '';
-    document.getElementById('uf').value = ''; 
-}  
+    document.getElementById('uf').value = '';
+}
 
 
